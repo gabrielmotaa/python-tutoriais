c778e630ecf555f99884e76a1dde756a127c26a3
Gabriel Mota <gabrielmotaa@hotmail.com>
2021-10-26 14:02:49 -0300

    Refatoração de testes com 'parametrize'

--- README.md
+++ README.md
@@ -1,10 +1,8 @@
 # Testes em Python
 Nessas branchs de `testes-x`, vamos entender como criar testes em Python utilizando o framework Pytest e algumas ferramentas da biblioteca padrão. 
 
-# Teste 1
-Vamos começar por um pequeno script que é capaz de produzir uma fita complementar de segmentos de DNA.
-
-O script vai se chamar `snps_util.py`, pois, conforme o projeto for avançando, vamos criar uma pequena biblioteca com funções de nos ajudar a analisar arquivos de SNPs.
+# Teste 2
+Com os primeiros testes criados, vimos como há muita repetição no código. Testes, assim como os códigos normais, também devem evitar repetições. O Pytest nos fornece ferramentas para isso, nesse caso o decorador `@pytest.mark.parametrize`.
 
 ## Instruções
 Clone o repositório em seu computador e rode os seguintes comandos:
--- test_snps_util.py
+++ test_snps_util.py
@@ -3,19 +3,23 @@ from snps_util import complementary_strand
 import pytest
 
 
-def test_complementary_strand_success():
+@pytest.mark.parametrize(
+    "sequence,result",
+    [
+        ('AAATTTCCGG', 'TTTAAAGGCC'),
+        ('A','T'),
+        ('CATCATGATACC', 'GTAGTACTATGG')
+    ]
+)
+def test_complementary_strand_success(sequence, result):
     """Deve retornar sequências de DNA invertidas corretamente"""
-    seq1, res1 = 'AAATTTCCGG', 'TTTAAAGGCC'
-    seq2, res2 = 'A','T'
-    seq3, res3 = 'CATCATGATACC', 'GTAGTACTATGG'
-
-    assert complementary_strand(seq1) == res1
-    assert complementary_strand(seq2) == res2
-    assert complementary_strand(seq3) == res3
+    assert complementary_strand(sequence) == result
 
 
-def test_complementary_strand_failure():
+@pytest.mark.parametrize(
+    "incorrect_sequence",
+    [(1,), (True,), ([],)]
+)
+def test_complementary_strand_failure(incorrect_sequence):
     """Deve estourar um TypeError se uma string não for passada."""
-    pytest.raises(TypeError, complementary_strand, 1)
-    pytest.raises(TypeError, complementary_strand, True)
-    pytest.raises(TypeError, complementary_strand, [])
+    pytest.raises(TypeError, complementary_strand, incorrect_sequence)
