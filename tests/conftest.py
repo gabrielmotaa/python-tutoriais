f5085d892311228c544a24bcf6a7d04a0309bcea
Gabriel Mota <gabrielmotaa@hotmail.com>
2021-10-26 16:32:15 -0300

    Parametrizado fixtures

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,33 +3,25 @@ import tempfile
 
 import pytest
 
-
-@pytest.fixture
-def ftdna_file():
-    content = [
-        "RSID,CHROMOSOME,POSITION,RESULT\n",
-        "rs123,1,12345,GG\n",
-        "rs4312,2,453231,AT\n",
-    ]
-    _, tmp_path = tempfile.mkstemp(suffix='.csv')
-
-    with open(tmp_path, 'w') as f:
-        f.writelines(content)
-
-    yield tmp_path
-
-    os.unlink(tmp_path)
+contents = {
+    'ftdna': ([
+                  "RSID,CHROMOSOME,POSITION,RESULT\n",
+                  "rs123,1,12345,GG\n",
+                  "rs4312,2,453231,AT\n",
+              ], '.csv'),
+    '23andme': ([
+                    "#rsid\tchromosome\tposition\tgenotype\n",
+                    "rs123\t1\t12345\tGG\n",
+                    "rs4312\t2\t453231\tAT\n",
+                ], '.txt'),
+}
 
 
-@pytest.fixture
-def twentythreeandme_file():
-    content = [
-        "#rsid\tchromosome\tposition\tgenotype\n",
-        "rs123\t1\t12345\tGG\n",
-        "rs4312\t2\t453231\tAT\n",
-    ]
+@pytest.fixture(params=["ftdna", "23andme"])
+def snp_file(request):
+    content, extension = contents[request.param]
 
-    _, tmp_path = tempfile.mkstemp(suffix='.txt')
+    _, tmp_path = tempfile.mkstemp(suffix=extension)
 
     with open(tmp_path, 'w') as f:
         f.writelines(content)
--- tests/test_files.py
+++ tests/test_files.py
@@ -19,33 +19,24 @@ class TestSNPFile:
     def test_discover_reader_error(self, filename):
         pytest.raises(Exception, SNPFile, filename)
 
-    def test_get_genotype_success_ftdna(self, ftdna_file):
-        snp_file = SNPFile(ftdna_file)
+    def test_get_genotype_success(self, snp_file):
+        snp_file = SNPFile(snp_file)
         assert snp_file.get_genotype('rs4312') == 'AT'
 
-    def test_get_genotype_success_23andme(self, twentythreeandme_file):
-        snp_file = SNPFile(twentythreeandme_file)
-        assert snp_file.get_genotype('rs4312') == 'AT'
-
-    def test_get_genotype_non_existent_ftdna(self, ftdna_file):
-        snp_file = SNPFile(ftdna_file)
+    def test_get_genotype_non_existent(self, snp_file):
+        snp_file = SNPFile(snp_file)
         pytest.raises(Exception, snp_file.get_genotype, 'rs11111')
 
-    def test_get_genotype_non_existent_23andme(self, twentythreeandme_file):
-        snp_file = SNPFile(twentythreeandme_file)
-        pytest.raises(Exception, snp_file.get_genotype, 'rs11111')
-
-    def test_get_genotype_complementary_ftdna(self, ftdna_file):
-        snp_file = SNPFile(ftdna_file)
-        assert snp_file.get_genotype('rs123', complementary=True) == 'CC'
-
-    def test_get_genotype_complementary_23andme(self, twentythreeandme_file):
-        snp_file = SNPFile(twentythreeandme_file)
+    def test_get_genotype_complementary(self, snp_file):
+        snp_file = SNPFile(snp_file)
         assert snp_file.get_genotype('rs123', complementary=True) == 'CC'
 
 
-def test_read_ftdna(ftdna_file):
-    data = _read_ftdna(ftdna_file)
+def test_read_vendor(snp_file):
+    if snp_file.endswith('.csv'):
+        data = _read_ftdna(snp_file)
+    elif snp_file.endswith('.txt'):
+        data = _read_23andme(snp_file)
 
     assert next(data) == {'rsid': 'rs123', 'chrom': '1', 'pos': '12345', 'result': 'GG'}
     assert next(data) == {'rsid': 'rs4312', 'chrom': '2', 'pos': '453231', 'result': 'AT'}
