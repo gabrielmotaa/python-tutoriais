fbcab06c902ec35a10b5597a5e39c501e3a409ca
Gabriel Mota <gabrielmotaa@hotmail.com>
2021-10-26 13:56:17 -0300

    Teste de falha criado e script modificado

--- snps_util.py
+++ snps_util.py
@@ -1,4 +1,6 @@
 def complementary_strand(sequence):
     """Recebe uma sequência de DNA e retorna a fita complementar."""
+    if not isinstance(sequence, str):
+        raise TypeError('Apenas strings podem ser passadas como argumento.')
     translator = str.maketrans('ATCG', 'TAGC')
     return sequence.translate(translator)
--- test_snps_util.py
+++ test_snps_util.py
@@ -1,5 +1,7 @@
 from snps_util import complementary_strand
 
+import pytest
+
 
 def test_complementary_strand_success():
     """Deve retornar sequências de DNA invertidas corretamente"""
@@ -10,3 +12,10 @@ def test_complementary_strand_success():
     assert complementary_strand(seq1) == res1
     assert complementary_strand(seq2) == res2
     assert complementary_strand(seq3) == res3
+
+
+def test_complementary_strand_failure():
+    """Deve estourar um TypeError se uma string não for passada."""
+    pytest.raises(TypeError, complementary_strand, 1)
+    pytest.raises(TypeError, complementary_strand, True)
+    pytest.raises(TypeError, complementary_strand, [])
